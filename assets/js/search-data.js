5fa81bed1a94f556e99d8eabef999e5e7bf1a2ee
hxtunq <hxtunq@gmail.com>
2025-05-01 07:16:36 +0000

    Deploying to gh-pages from @ hxtunq/hxtunq.github.io@6bbca20bf3a9562b805ade497cb9b10afacca6f9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -265,8 +265,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-became-a-huster",
+          title: 'Became a HUSTer!',
           description: "",
           section: "News",},{id: "projects-bioinformatics",
           title: 'Bioinformatics',
